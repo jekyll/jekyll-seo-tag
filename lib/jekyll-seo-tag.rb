546448be1b8f81aee4fa2fd708c817cdb5975f1d
Ben Balter <ben.balter@github.com>
2016-02-20 15:49:35 -0500

    move logic to ruby

--- lib/jekyll-seo-tag.rb
+++ lib/jekyll-seo-tag.rb
@@ -17,10 +17,18 @@ module Jekyll
 
     def payload
       {
-        'seo_tag' => { 'version' => VERSION },
-        'page'    => context.registers[:page],
-        'site'    => context.registers[:site].site_payload['site']
+        'seo_tag' => { 'version' => VERSION, 'author' => author },
+        'page'    => page,
+        'site'    => site
       }
+    end
+
+    def page
+      context.registers[:page]
+    end
+
+    def site
+      context.registers[:site].site_payload['site']
     end
 
     def info
@@ -43,6 +51,28 @@ module Jekyll
     def template_path
       @template_path ||= begin
         File.expand_path './template.html', File.dirname(__FILE__)
+      end
+    end
+
+    # Returns a hash representing the post author
+    #
+    # Sources, in order:
+    #
+    # 1. page.author, if page.author is a hash
+    # 2. site.author, if site.author is a hash
+    # 3. site.data.authors[page.author] if page.author is a string
+    # 4. page.author if page.author is a string
+    def author
+      author = page['author'] || site['author']
+      return if author.nil?
+      return author if author.is_a?(Hash)
+
+      if author.is_a?(String)
+        if site['data']['authors'] && site['data']['authors'][author]
+          site['data']['authors'][author]
+        else
+          { 'twitter' => author }
+        end
       end
     end
   end
